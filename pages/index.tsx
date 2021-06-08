6cb640ab5b9368feae8a34bf9df0d58637349ef5
Brady Blair <thevillemc@gmail.com>
2021-06-08 19:57:21 -0400

    adjust settings to use ssr with styled components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,5 @@
-import styled, { ThemeProvider } from 'styled-components';
-import { mainTheme } from '../styles/theme';
 import Header from '../components/Header';
 
 export default function Home() {
-  return (
-    <ThemeProvider theme={mainTheme}>
-      <Header />
-    </ThemeProvider>
-  );
+  return <Header />;
 }
