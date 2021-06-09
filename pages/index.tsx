96dccf367f058d785ab43dcc19eae835fb45e720
Brady Blair <thevillemc@gmail.com>
2021-06-09 06:15:48 -0400

    add planet and information component to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import Header from '../components/Header';
 import SectionMenu from '../components/SectionMenu';
+import Planet from '../components/Planet';
+import Information from '../components/Information';
 
 export default function Home() {
   return (
@@ -7,6 +9,8 @@ export default function Home() {
       <Header />
       <main>
         <SectionMenu />
+        <Planet />
+        <Information />
       </main>
     </>
   );
