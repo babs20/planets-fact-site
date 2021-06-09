69edcb489680925224b2269092a14975d68427d1
Brady Blair <thevillemc@gmail.com>
2021-06-09 07:31:50 -0400

    add container to main page for flex spacing

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,16 @@ import Header from '../components/Header';
 import SectionMenu from '../components/SectionMenu';
 import Planet from '../components/Planet';
 import Information from '../components/Information';
+import Facts from '../components/Facts';
+import styled from 'styled-components';
+
+const PlanetContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  padding: 0 24px;
+`;
 
 export default function Home() {
   return (
@@ -9,8 +19,11 @@ export default function Home() {
       <Header />
       <main>
         <SectionMenu />
-        <Planet />
-        <Information />
+        <PlanetContainer>
+          <Planet />
+          <Information />
+          <Facts />
+        </PlanetContainer>
       </main>
     </>
   );
