c1b47869f40d6a9b495db4dda93aca01c37e8860
Brady Blair <thevillemc@gmail.com>
2021-06-13 02:53:50 -0400

    add desktop planet size

--- styled.d.ts
+++ styled.d.ts
@@ -31,34 +31,42 @@ declare module 'styled-components' {
       planetDimensions: {
         mercury: {
           mobile: readonly SimpleInterpolation[];
+          tablet: readonly SimpleInterpolation[];
           desktop: readonly SimpleInterpolation[];
         };
         venus: {
           mobile: readonly SimpleInterpolation[];
+          tablet: readonly SimpleInterpolation[];
           desktop: readonly SimpleInterpolation[];
         };
         earth: {
           mobile: readonly SimpleInterpolation[];
+          tablet: readonly SimpleInterpolation[];
           desktop: readonly SimpleInterpolation[];
         };
         mars: {
           mobile: readonly SimpleInterpolation[];
+          tablet: readonly SimpleInterpolation[];
           desktop: readonly SimpleInterpolation[];
         };
         jupiter: {
           mobile: readonly SimpleInterpolation[];
+          tablet: readonly SimpleInterpolation[];
           desktop: readonly SimpleInterpolation[];
         };
         saturn: {
           mobile: readonly SimpleInterpolation[];
+          tablet: readonly SimpleInterpolation[];
           desktop: readonly SimpleInterpolation[];
         };
         uranus: {
           mobile: readonly SimpleInterpolation[];
+          tablet: readonly SimpleInterpolation[];
           desktop: readonly SimpleInterpolation[];
         };
         neptune: {
           mobile: readonly SimpleInterpolation[];
+          tablet: readonly SimpleInterpolation[];
           desktop: readonly SimpleInterpolation[];
         };
       };
