c462bd08b4a239494ff453bd7cd279a2cf1e80b9
Brady Blair <thevillemc@gmail.com>
2021-06-13 05:19:28 -0400

    add lang tag to html and name to manifest

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -35,7 +35,7 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang='en'>
         <Head>
           <link rel='preconnect' href='https://fonts.gstatic.com' />
           <link
